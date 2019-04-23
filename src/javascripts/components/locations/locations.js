05f686d47ad2619bb38f266abf8b74118e4150f9
Bob <bob.e.baxter@gmal.com>
2019-04-22 19:26:36 -0500

    finished switch statement for card generation

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -5,12 +5,33 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-success';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-inf0';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-danger';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
 const domStringBuilder = () => {
   let domString = '';
   locations.forEach((location) => {
     domString += `<div id="${locations.id}"class="card location col-2" style="width: 18rem;">`;
+    domString += `<h5 class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</h5>`;
     domString += '<div class="card-body">';
-    domString += `<h5 class="card-header">${location.name}</h5>`;
     domString += `<img src=${location.imageUrl} class="card-img-top">`;
     domString += `<p class="card-text"><small>${location.address} Locations</small></p>`;
     domString += '</div>';
